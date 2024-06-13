223699f70d5196b6de6cc8d88d6b20d2514ae225
Hady Ayman <hadyaymman@gmail.com>
2024-06-13 08:02:07 +0300

    User Interface

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,26 +1,11 @@
-<?php
 
-function displayUsernameOrBrand() {
-  if (isset($_SESSION['user_type'])) {
-      if ($_SESSION['user_type'] === 'artist' || $_SESSION['user_type'] === 'craftsmen') {
-          return $_SESSION['brand_name'];
-      } else {
-          return $_SESSION['username'];
-      }
-  } else {
-      return 'View Profile';
-  }
-}
-?>
-
-<?php  include "./admin/fetch_categories.php";?>
 
 <header class="header" data-header>
     <div class="nav-container">
 
       <div class="overlay container-fluid" data-overlay></div>
 
-      <a href="index.php" class="logo navbar-brand">
+      <a href="index.html" class="logo navbar-brand">
         ArtRegalia
       </a>
 
@@ -34,7 +19,7 @@ function displayUsernameOrBrand() {
           <i class="close-outline fa-solid fa-xmark"></i>
         </button>
 
-        <a href="index.php" class="nav-logo">
+        <a href="index.html" class="nav-logo">
           ArtRegalia
         </a>
 
@@ -43,68 +28,61 @@ function displayUsernameOrBrand() {
       
 
           <li class="navbar-item nav-item">
-            <a href="index.php" class="navbar-links nav-link">Home</a>
+            <a href="index.html" class="navbar-links nav-link">Home</a>
           </li>
 
 
 
           <li class="navbar-item nav-item">
-            <a href="Artists-page.php" class="navbar-links desk nav-link">Artists</a>
+            <a href="Artists-page.html" class="navbar-links desk nav-link">Artists</a>
             <input type="checkbox" id="showArtists" class="dropdown-toggle">
             <label for="showArtists" class="navbar-links">Artists <i class="fa-solid fa-caret-down"></i></label>
             <ul class="drop-menu">
-            <?php 
-
-             foreach($categories['artist']['main'] as $artistCategory):
-             echo "<li><a class='dropdown-item' href='#'> {$artistCategory['category_title']} </a></li>";
-             endforeach;
-            ?>
+              <li><a class='dropdown-item' href='#'>Street Artist</a></li>
             </ul>
           </li>
 
           <li class="navbar-item nav-item">
-            <a href="craft-page.php" class="navbar-links desk nav-link">Craftsmen</a>
+            <a href="craft-page.html" class="navbar-links desk nav-link">Craftsmen</a>
             <input type="checkbox" id="showMega" class="dropdown-toggle">
             <label for="showMega" class="navbar-links">Craftsmen<i class="fa-solid fa-caret-down"></i></label>
             <div class="mega-box">
               <div class="content">
-              <?php foreach ($categories['craftsmen']['main'] as $mainCategory): ?>
+              
                 <div class="row">
-                <header><?php echo $mainCategory['category_title']; ?></header>
+                <header>Bags</header>
                   <ul class="mega-links">
-                  <?php if (isset($categories['craftsmen']['sub'][$mainCategory['category_id']])): ?>
-                            <?php foreach ($categories['craftsmen']['sub'][$mainCategory['category_id']] as $subCategory): ?>
-                                <li><a class="dropdown-item" href="#"><?php echo $subCategory['category_title']; ?></a></li>
-                            <?php endforeach; ?>
-                        <?php endif; ?>
+                    <li><a class="dropdown-item" href="#">beaded</a></li>    
+                    <li><a class="dropdown-item" href="#">beaded</a></li>    
+                    <li><a class="dropdown-item" href="#">beaded</a></li>    
+                    <li><a class="dropdown-item" href="#">beaded</a></li>    
+                    <li><a class="dropdown-item" href="#">beaded</a></li>    
                   </ul>
                 </div>
-                <?php endforeach; ?>
+              
               </div>
             </div>
           </li>
 
           <li class="navbar-item nav-item">
-            <a href="shopall.php" class="navbar-links nav-link">Shop all</a>
+            <a href="shopall.html" class="navbar-links nav-link">Shop all</a>
           </li>
 
           <li class="navbar-item nav-item">
-            <a href="shops.php" class="navbar-links nav-link">Shops</a>
+            <a href="shops.html" class="navbar-links nav-link">Shops</a>
           </li>
 
           <li class="navbar-item nav-item">
-            <a href="community.php" class="navbar-links nav-link">Community</a>
+            <a href="community.html" class="navbar-links nav-link">Community</a>
           </li>
 
           <li class="navbar-item nav-item">
-            <a href="about.php" class="navbar-links nav-link">About Us</a>
+            <a href="about.html" class="navbar-links nav-link">About Us</a>
           </li>
 
-            <?php if (isset($_SESSION['user_type']) && in_array($_SESSION['user_type'], ['admin'])): ?>
           <li class="navbar-item nav-item">
-            <a href="./admin/admin_home.php" class="navbar-links nav-link">Admin</a>
+            <a href="./admin/admin_home.html" class="navbar-links nav-link">Admin</a>
           </li>
-          <?php endif; ?>
 
         </ul>
 
@@ -131,7 +109,7 @@ function displayUsernameOrBrand() {
           </li>
 
           <li class="nav-item ms-2">
-            <a href="cart.php" class="nav-action-btn nav-link">
+            <a href="cart.html" class="nav-action-btn nav-link">
               <i class="fa-solid fa-bag-shopping" aria-hidden="true"></i>
               <span class="icon-badge-btn">4</span>
               <data class="nav-action-text" value="400.00">Basket: <strong>400EGP</strong></data>
@@ -147,15 +125,12 @@ function displayUsernameOrBrand() {
             <label for="showProfile" class="navbar-links"><i class="fa-solid fa-user" aria-hidden="true"></i>Profile <i
                 class="fa-solid fa-caret-down"></i></label>
             <ul class="drop-menu profile">
-              <li><a class="nav-link" href="#"><i class="fa-solid fa-user"></i><?php echo displayUsernameOrBrand();?></a></li>
-              <li><a class="nav-link" href="#"><i class="fa-solid fa-heart"></i>Wishlist</a></li>
-              <li><a class="nav-link" href="#"><i class="fa-solid fa-circle-question"></i>Help</a></li>
-              <li><a class="nav-link" href="#"><i class="fa-solid fa-gears"></i>Settings</a></li>
-              <?php if (isset($_SESSION['username'])): ?>
-                            <li><a class="nav-link" href="logout.php"><i class="fa-solid fa-lock"></i>Sign-out</a></li>
-                        <?php else: ?>
-                            <li><a class="nav-link" href="login.php"><i class="fa-solid fa-lock"></i>Sign-in/Sign-up</a></li>
-                        <?php endif; ?>
+              <li><a class="nav-link" href="./user-profile.html"><i class="fa-solid fa-user"></i>View Profile</a></li>
+              <li><a class="nav-link" href="./wishlist.html"><i class="fa-solid fa-heart"></i>Wishlist</a></li>
+              <li><a class="nav-link" href="help.html"><i class="fa-solid fa-circle-question"></i>Help</a></li>
+              <li><a class="nav-link" href="settings.html"><i class="fa-solid fa-gears"></i>Settings</a></li>
+            
+              <li><a class="nav-link" href="login.html"><i class="fa-solid fa-lock"></i>Sign-in/Sign-up</a></li>
             </ul>
           </li>
         </ul>
